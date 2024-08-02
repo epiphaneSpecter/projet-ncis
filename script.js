822e756b6a84ed3898e7c3a89be865b2d3ad23e4
Epiphane AVLAH <epiavlah@gmail.com>
2024-08-02 00:28:14 -0400

    wip

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   new Splide("#software-carousel", {    
     perPage: 4,
     breakpoints: {
-      640: {
+      600: {
         perPage: 3,
+        gap: 70
       },
     },
     gap: 250,
@@ -13,20 +14,44 @@ document.addEventListener("DOMContentLoaded", function () {
     perPage: 3,
     pagination: false,
     breakpoints: {
-      640: {
+      600: {
         perPage: 1,
       },
     },
-    gap: 95,
+    gap: 65,
   }).mount();
 
-  const links = document.querySelectorAll('nav.header-nav--desktop a.menu');
+  const links = document.querySelectorAll('nav a.menu');
+  const menu = document.getElementById('header-nav--mobile');
+
     for (const link of links) {
         link.addEventListener('click', function(event) {
             event.preventDefault();
+
+            if(this.classList.contains('mobile-menu')) {
+                menu.classList.remove('active');
+            }
             const targetId = this.getAttribute('href').substring(1);
             const targetSection = document.getElementById(targetId);
             targetSection.scrollIntoView({ behavior: 'smooth' });
         });
     }
+
+    const menuIcon = document.getElementById('mobile-menu-trigger');
+    const closeIcon = document.getElementById('close-icon');
+    
+
+    menuIcon.addEventListener('click', () => {
+        menu.classList.add('active');
+    });
+
+    closeIcon.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target == menu) {
+            menu.classList.remove('active');
+        }
+    });
 });
